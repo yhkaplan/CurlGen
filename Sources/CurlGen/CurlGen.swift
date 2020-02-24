3126f94cfaa5d895b50d91d3774cb2065b0c0627
Joshua Kaplan <yhkaplan@gmail.com>
2020-02-24 15:31:22 +0900

    Refactor: break apart operations into methods for readability

--- Sources/CurlGen/CurlGen.swift
+++ Sources/CurlGen/CurlGen.swift
@@ -1,51 +1,69 @@
 import Foundation
 
-public extension URLRequest {
+extension URLRequest {
 
     private var base: String { "curl -v " }
-
-    var cURL: String { // TODO: turn into func accepting isVerbose bool
+    
+    private typealias Parameter = (flag: String, value: String?)
+    
+    /// TODO: document
+    public var cURL: String { // TODO: turn into func accepting isVerbose bool
         guard let urlString = url?.absoluteString else { return "" }
+        
         let base = "curl -v " + urlString
+        let headerParameters = makeHeaderParameters()
+        let bodyParameter = makeBodyParameter()
+        let methodParameter = (flag: "-X", value: httpMethod)
+        
+        var rawParameters: [Parameter] = [methodParameter]
+        rawParameters.append(contentsOf: headerParameters)
+        rawParameters.append(bodyParameter) // TODO: reverse body and header position
 
-        let sortedHeaders = allHTTPHeaderFields?
-            .map { (key: $0.key, value: $0.value) }
+        let joinedParameters = joinParameters(rawParameters)
+        
+        return joinedParameters.isEmpty
+            ? base
+            : base + " " + joinedParameters
+    }
+
+}
+
+// MARK: - Helper Methods
+extension URLRequest {
+
+    private func makeHeaderParameters() -> [Parameter] {
+        let sortedHeaders = allHTTPHeaderFields? // Sorting headers for consistency with tests
             .sorted(by: { $0.key < $1.key })
             ?? []
-        let headerParameters = sortedHeaders
-            .map { (flag: "--header", value: Optional("\"\($0.key)=\($0.value)\"")) }
+        return sortedHeaders.map { (flag: "--header", value: Optional("\"\($0.key)=\($0.value)\"")) }
+    }
+    
+    private func makeBodyParameter() -> Parameter {
+        guard let data = httpBody else { return ("", nil) }
+        
+        let contentType = value(forHTTPHeaderField: "Content-Type")
+        
+        var body: String?
+        switch contentType {
+        case "application/x-www-form-urlencoded", "application/json":
+            body = String(data: data, encoding: .utf8)
+            
+        case "multipart/form-data":
+            body = "<multipart-body>"
+            
+        default:
+            body = "<unknown body>"
+        }
 
-        var rawParameters: [(flag: String, value: String?)] = [
-            (flag: "-X", value: httpMethod),
-        ]
-        rawParameters.append(contentsOf: headerParameters)
+        return ("--data", body)
+    }
 
-        let formattedParameters = rawParameters
+    private func joinParameters(_ parameters: [Parameter]) -> String {
+        return parameters
             .compactMap { parameter in
                 guard let value = parameter.value else { return nil }
                 return parameter.flag + " " + value
             }
             .joined(separator: " \\\n")
-
-        var body = ""
-        if let data = httpBody {
-            let contentType = value(forHTTPHeaderField: "Content-Type")
-            switch contentType {
-            case "application/x-www-form-urlencoded", "application/json":
-                guard let bodyString = String(data: data, encoding: .utf8) else { return "" } // TODO: throw error?
-                body = " \\\n--data " + bodyString
-                
-            case "multipart/form-data":
-                body = " \\\n--data <multipart-body>"
-                
-            default:
-                body = " \\\n--data <unknown body>"
-            }
-        }
-        
-        return formattedParameters.isEmpty
-            ? base
-            : base + " " + formattedParameters + body
     }
-
 }
